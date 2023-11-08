70c95d52867d2428cc836fdff75b640e37a286aa
Dirk Arnez <smalldirkalex@gmail.com>
2023-11-08 20:26:21 +0800

    - update make-portable-installation.cmd

--- make-portable-installation.cmd
+++ make-portable-installation.cmd
@@ -2,5 +2,5 @@ set PATH=%PATH%;%USERPROFILE%\Downloads\strawberry-perl-5.38.0.1-64bit-portable\
 
 
 dir &&^
-.\miktexsetup_standalone.exe download --package-set=basic  --use-registry=no --local-package-repository="%USERPROFILE%\123" &&^
-.\miktexsetup_standalone.exe install --verbose --shared=no --portable="%USERPROFILE%\installation" --package-set=basic  --modify-path=no --use-registry=no --local-package-repository="%USERPROFILE%\123"
+.\miktexsetup_standalone.exe download --package-set=basic  --use-registry=no --local-package-repository="%~dp0123" &&^
+.\miktexsetup_standalone.exe install --verbose --shared=no --portable="%~dp0installation" --package-set=basic  --modify-path=no --use-registry=no --local-package-repository="%~dp0123"
